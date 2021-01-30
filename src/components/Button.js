36b2739f519412b2734367141967b2ae4b1dbe70
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-30 23:29:27 +1100

    btn state in header

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,27 @@
-const Button = ({ color, text }) => {
+import PropTypes from "prop-types";
+
+const Button = ({ color, text, onClick }) => {
   return (
     <div>
-      <button style={{ backgroundColor: color }} className="btn">
+      <button
+        onClick={onClick}
+        style={{ backgroundColor: color }}
+        className="btn"
+      >
         {text}
       </button>
     </div>
   );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
