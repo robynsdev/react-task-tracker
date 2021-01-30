93a8968b01cf56b69348ace47c3e72f845eb0193
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-30 23:18:19 +1100

    added button and index.css

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+const Button = ({ color, text }) => {
+  return (
+    <div>
+      <button style={{ backgroundColor: color }} className="btn">
+        {text}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
