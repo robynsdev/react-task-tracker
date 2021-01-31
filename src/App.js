f9b53aa510b211ecb2a2bfad481ecfb400155cf2
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-31 12:21:59 +1100

    tasks and deleteTask

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "cleaning",
+      day: "Feb 2th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "feeding yaya",
+      day: "Feb 3th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: "studying",
+      day: "Feb 4th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
-    <div className="Container">
+    <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
