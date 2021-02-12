7826994cbb0dd70fdf259221e7b1579978e30850
robyn <65057146+robynsdev@users.noreply.github.com>
2021-02-13 09:37:01 +1100

    fixed jest error and added show/hide functionality for addTask button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddTask from "./components/AddTask";
 import { useState } from "react";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -48,8 +49,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
